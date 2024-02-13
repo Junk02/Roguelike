7517d626a257e8a7239dd87b3c53e905e4f9abc3
klesh <klesh@DESKTOP-OCCNASI>
2024-02-13 17:02:46 +0300

    Добавьте файлы проекта.

---
+++ Roguelike/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelike
+{
+    internal class Game
+    {
+        public Game()
+        {
+            Console.WriteLine("Начало игры...");
+            Console.Write("Как тебя будут звать?");
+        }
+    }
+}
---
+++ Roguelike/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelike
+{
+    internal class Player
+    {
+        public string name { private set; get; }
+
+        public Player(string name)
+        {
+            this.name = name;
+        }
+    }
+}
